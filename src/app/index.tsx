43f1a1d8a48e069b1bcbe94b1374afec5ece73f1
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-14 18:29:37 +0530

    feat: created a simple counter for interactivity with buttons

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,11 @@
-import React from "react";
-import { View } from "react-native";
-import { Age, Clock, Greet, Name, SocialMedia, Subject, TextComponent } from "../components";
+import React, { useState } from "react";
+import { Button, View } from "react-native";
+import { Age, Name, SocialMedia, Subject, TextComponent } from "../components";
 
 export default function Index() {
+    const [count, setCount] = useState(0);
+    const increment = () => setCount(prev => prev + 1);
+    const decrement = () => setCount(prev => prev - 1);
     return (
         <View
             style={{
@@ -16,7 +19,10 @@ export default function Index() {
             <Name name="Hanish" />
             <Age age={19} />
             <SocialMedia platform="Instagram" username="hanish.dev" />
-            <Subject subject="Computer Science"/>
+            <Subject subject="Computer Science" />
+            <Button color={"teal"} title="Increment" onPress={increment} />
+            <TextComponent text={`Count: ${count}`} />
+            <Button color={"teal"} title="Decrement" onPress={decrement} disabled={count === 0} />
         </View>
     );
 };
