83ef6498cc1d458ba0ea84f0b7e28b5a3c21e2ed
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-14 17:41:33 +0530

    feat: updated component epxorting file with new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 export * from "./clock";
 export * from "./greet";
 export * from "./text-component";
+export * from "./name";
+export * from "./age";
+export * from "./social-media";
+export * from "./subject";
