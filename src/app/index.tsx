8bb6742861af3279e81bc4dc595f1d576bc1603d
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-14 17:05:59 +0530

    refactor: imported the components and used in index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,32 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { Text, View } from "react-native";
-
-const TextComponent = ({ text }: { text: string }) => {
-    return <Text>{text}</Text>;
-};
-
-const Greet = ({ name }: { name: string }) => { 
-    return <TextComponent text={`Welcome ${name} 👋!!!`} />
-};
-
-const Clock = () => {
-    const timeString = (date: Date) => date.toLocaleTimeString("en-US",{
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        hour12: true
-    })
-    const [time, setTime] = useState(timeString(new Date));
-    useEffect(() => {
-        const timerId = setInterval(() => {
-            setTime(timeString(new Date));
-        }, 1000);
-        return () => clearInterval(timerId);
-    }, []);
-    return (
-        <Text>{time}</Text>
-    );
-};
+import React from "react";
+import { View } from "react-native";
+import { Clock, Greet, TextComponent } from "../components";
 
 export default function Index() {
     const today = new Date();
