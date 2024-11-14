40a10b103751c3e727cb4f8decc5dd30ec898693
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-14 16:31:49 +0530

    feat: created a simple text component and passing props

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Text, View } from "react-native";
 
+const TextComponent = ({ text }: { text: string }) => {
+    return <Text>{text}</Text>;
+}
+
 export default function Index() {
     return (
         <View>
-            <Text>Hello World</Text>
+            <TextComponent text="Hello World" />
         </View>
     );
 };
