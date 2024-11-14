6c9f9990604903528f8d6ce50fc190e1a1acd229
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-14 17:41:06 +0530

    feat: created a social-media component

---
+++ src/components/social-media.tsx
@@ -0,0 +1,11 @@
+import { TextComponent } from "./text-component";
+
+export const SocialMedia = ({
+    platform,
+    username
+}: {
+    platform: string;
+    username: string
+}) => {
+    return <TextComponent text={`Here is my ${platform} handle: ${username}`} />
+};
