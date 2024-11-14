73960fbd054999ce7dd344580650aace8a5a55be
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-14 16:42:48 +0530

    feat: just added a function show a simple dynamic value

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,9 +10,22 @@ const Greet = ({ name }: { name: string }) => {
 };
 
 export default function Index() {
+    const today = new Date();
+    const currentHour = today?.getHours();
+    const getCurrentTime = (hour: number) => {
+        switch (true) {
+            case (hour < 12):
+                return "Morning";
+            case (hour < 16):
+                return "Afternoon";
+            default:
+                return "Evening";
+        }
+    }
     return (
         <View>
-            <Greet name="Hanish"/>
+            <Greet name="Hanish" />
+            <TextComponent text={`Good ${getCurrentTime(currentHour)}`} />
         </View>
     );
 };
