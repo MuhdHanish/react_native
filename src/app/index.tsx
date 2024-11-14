e6c81382f26fe7aac60edeaf2699b0c079138177
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-14 17:00:59 +0530

    feat: just created a clock component for using the useEffect and useState hooks

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 const TextComponent = ({ text }: { text: string }) => {
@@ -7,6 +7,25 @@ const TextComponent = ({ text }: { text: string }) => {
 
 const Greet = ({ name }: { name: string }) => { 
     return <TextComponent text={`Welcome ${name} 👋!!!`} />
+};
+
+const Clock = () => {
+    const timeString = (date: Date) => date.toLocaleTimeString("en-US",{
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: true
+    })
+    const [time, setTime] = useState(timeString(new Date));
+    useEffect(() => {
+        const timerId = setInterval(() => {
+            setTime(timeString(new Date));
+        }, 1000);
+        return () => clearInterval(timerId);
+    }, []);
+    return (
+        <Text>{time}</Text>
+    );
 };
 
 export default function Index() {
@@ -34,6 +53,7 @@ export default function Index() {
         >
             <Greet name="Hanish" />
             <TextComponent text={`Good ${getCurrentTime(currentHour)}`} />
+            <Clock />
         </View>
     );
 };
