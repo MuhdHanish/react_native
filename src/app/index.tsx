2839efaca16e13e08cf4deddf9270c765dc9e914
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-15 21:21:53 +0530

    feat: mapped car card

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,33 @@
 import React from "react";
 import { View } from "react-native";
-import { Profile } from "../components";
+import { CarCard } from "../components";
+
+const cars = [
+    // {
+    //     id: 1,
+    //     price: 200_000,
+    //     imageUri: "https://img.freepik.com/free-photo/front-view-black-sedan-sport-car-bridge_114579-4068.jpg?ga=GA1.1.1279576087.1717824931&semt=ais_hybrid",
+    //     name: "Sedan X200",
+    //     rating: 4.5,
+    //     description: "A compact sedan with excellent fuel efficiency and modern features."
+    // },
+    // {
+    //     id: 2,
+    //     price: 450_000,
+    //     imageUri: "https://img.freepik.com/free-photo/luxurious-car-parked-highway-with-illuminated-headlight-sunset_181624-60607.jpg?ga=GA1.1.1279576087.1717824931&semt=ais_hybrid",
+    //     name: "SUV MaxPro",
+    //     rating: 4.8,
+    //     description: "A spacious SUV with advanced safety features and all-wheel drive."
+    // },
+    {
+        id: 3,
+        price: 350_000,
+        imageUri: "https://img.freepik.com/free-photo/yellow-sport-sedan-road-side-view_114579-5041.jpg?ga=GA1.1.1279576087.1717824931&semt=ais_hybrid",
+        name: "Coupe SportZ",
+        rating: 4.7,
+        description: "A stylish coupe offering top performance and sleek design."
+    }
+];
 
 export default function Index() {
     return (
@@ -13,15 +40,11 @@ export default function Index() {
                 gap: "10px"
             }}
         >
-            <Profile
-                name="hanish.dev"
-                bio={`
-                • Software Engineer
-                • https://github.com/MuhdHanish
-                • https://www.linkedin.com/in/muhdhanish
-                `}
-                profile={require("../assets/Decrease_3.jpg")}
-            />
+            {
+                cars?.map((car, index) => (
+                    <CarCard {...car} key={`${car?.id}-${index}`} />
+                ))
+            }
         </View>
     );
 };
