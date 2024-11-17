29796fb10320deac9249cb934fb81ec70ad473d2
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-17 10:31:03 +0530

    feat: added safearea view

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,17 @@ import { View } from "react-native";
 import { AddRandomNumber, RandomNumber } from "../components/prop-drilling";
 import { AuthProvider } from "../context/useAuth";
 import { RandomNumbersProvider } from "../context/useRandomNumber";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
     return (
         <AuthProvider>
             <RandomNumbersProvider>
                 <View style={{ padding: 10 }}>
-                    <RandomNumber />
-                    <AddRandomNumber />
+                    <SafeAreaView>
+                        <RandomNumber />
+                        <AddRandomNumber />
+                    </SafeAreaView>
                 </View>
             </RandomNumbersProvider>
         </AuthProvider>
