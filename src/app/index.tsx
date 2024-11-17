67eab85b77aeafb23aece02b4f2b7d636a17f737
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-17 11:50:28 +0530

    feat: added nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,16 @@
-import { View } from "react-native";
-import { AuthProvider } from "../context/useAuth";
-import { RandomNumbersProvider } from "../context/useRandomNumber";
-import { MyForm } from "../components";
-
+import "../../global.css";
+import { SafeAreaView, StatusBar, Text, useColorScheme } from "react-native";
 export default function Index() {
     return (
-        <AuthProvider>
-            <RandomNumbersProvider>
-                <View style={{ padding: 10 }}>
-                    <MyForm />
-                </View>
-            </RandomNumbersProvider>
-        </AuthProvider>
+        <SafeAreaView>
+            <StatusBar
+                barStyle={"light-content"}
+                backgroundColor="transparent"
+                translucent
+            />
+            <Text className="bg-green-100 p-4 text-green-500 m-4 rounded">
+                Tailwind CSS in React Native (NativeWind)
+            </Text>
+        </SafeAreaView>
     );
-};
+}
